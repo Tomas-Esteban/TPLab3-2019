8814f2fecb3b92061ad1c7c79b7cb58817b9fc6f
ivanmdq22 <ivangraciarena@outlook.com>
2019-06-06 23:21:57 -0300

    creacion del uml

--- TP Final Programacion 3/src/Pantallas/Login.java
+++ TP Final Programacion 3/src/Pantallas/Login.java
@@ -1,19 +1,29 @@
 package Pantallas;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Login extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
 	 */
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -37,6 +47,24 @@ public class Login extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnNewButton.setBounds(175, 175, 114, 25);
+		contentPane.add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(175, 81, 124, 19);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(175, 106, 124, 19);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
 	}
-
 }
